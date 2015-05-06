31fa95eb09b45ecd325701c3fba0d050fa792948
Kazuyuki Honda <hakobera@gmail.com>
2015-05-06 12:30:31 +0900

    Example of using mocha

--- test/helper.rb
+++ test/helper.rb
@@ -22,6 +22,7 @@ rescue Bundler::BundlerError => e
   exit e.status_code
 end
 require 'test/unit'
+require "mocha/test_unit"
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
--- test/plugin/test_out_google_cloud.rb
+++ test/plugin/test_out_google_cloud.rb
@@ -189,6 +189,7 @@ class GoogleCloudOutputTest < Test::Unit::TestCase
   end
 
   def test_gce_metadata_does_not_load_when_fetch_gce_metadata_is_false
+    Fluent::GoogleCloudOutput.any_instance.expects(:fetch_metadata).never
     d = create_driver(CUSTOM_METADATA_CONFIG)
     d.run
     assert_equal CUSTOM_PROJECT_ID, d.instance.project_id
